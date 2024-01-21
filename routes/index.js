a8825b3b85c48d4ac6958d051af5f0e56c071772
HPatto <hpatto97@gmail.com>
2024-01-21 19:14:39 -0300

    Update views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// Homepage text content
+const tabText = "STREAM OF CONSCIOUSNESS";
+const headerText = "STREAM OF CONSCIOUSNESS";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(
+    'index',
+    {
+      tabTitle: tabText,
+      header: headerText,
+      vibes: "We are now vibing"
+    }
+  );
 });
 
 module.exports = router;
