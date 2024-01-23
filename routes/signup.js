de0c3d1ccd8cfec7019c96122b5f85734271c423
HPatto <hpatto97@gmail.com>
2024-01-23 14:38:22 -0300

    Update filenames

--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET signup page. */
 router.get('/', function(req, res, next) {
-  res.render('signup');
+  res.render('signuppage');
 });
 
 module.exports = router;
