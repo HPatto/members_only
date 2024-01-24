b8c1a8426645696133c068fa71760857e787958f
HPatto <hpatto97@gmail.com>
2024-01-23 21:18:15 -0300

    Update file structure

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // Will need conditional logic here to serve up w.r.t. cookies.
   res.render('landingpage');
 });
 
