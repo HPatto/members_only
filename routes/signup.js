38579d92d57958245c20ee80c3bfc14b8c7155b9
HPatto <hpatto97@gmail.com>
2024-01-24 22:52:06 -0300

    Update back-end logic

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 /* GET signup page. */
 router.get('/', function(req, res, next) {
   res.render('signup');
 });
+
+/* POST new user. */
+router.post('/', function(req, res, next) {
+  // Check if everything is valid?
+  //// Is it an e-mail?
+  //// Is it already in use?
+  //// Is the password valid (Do we want to implement rules?)
+
+  // Send it to the database
+})
 
 module.exports = router;
